ee12d3531117f3731bd1d96553945c13af8c6281
JoSungYun <37609646+SungYoon-Jo@users.noreply.github.com>
2022-07-21 12:45:29 +0900

    Update process.php

--- process.php
+++ process.php
@@ -3,6 +3,7 @@
     require("lib/db.php");
     $conn = db_init($config["host"],$config["duser"],
                     $config["dpw"],$config["dname"]);
+                    
     $sql = "INSERT INTO topic (title,description,author,created)VALUES('".$_POST['title']."','".$_POST['description']."','".$_POST['author']."',now())";
 
 /*    $sql = "INSERT INTO topic(title,description,author,created)VALUES('$_POST[title]','$_POST[description]','$_POST[author]','$_POST[created]')";*/
